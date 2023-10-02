6d63b2156d9a03a6727d83b832bf2a0d1e29026f
mykhailo_naumenko <74735041+mikhail2574@users.noreply.github.com>
2023-10-02 12:39:56 +0200

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
 
 const ImageGallery = ({ items, setSrc, setModal }) => {
